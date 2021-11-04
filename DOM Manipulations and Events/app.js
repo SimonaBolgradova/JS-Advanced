c17545f60684b1e9e08add4cddded74bc754b9ab
Simona Bolgradova <75329832+SimonaBolgradova@users.noreply.github.com>
2021-11-04 09:49:25 +0200

    Delete app.js

--- DOM Manipulations and Events/app.js
+++
@@ -1,7 +0,0 @@
-function addItem() {
-    const text = document.getElementById('newItemText').value;
-    const liElement= document.createElement('li');
-    liElement.textContent=text;
-
-    document.getElementById('items').appendChild(liElement);
-}
