dc226ec389d8bd5a3ed411f9a151f34017c71f6f
Simona Bolgradova <75329832+SimonaBolgradova@users.noreply.github.com>
2021-11-04 09:46:07 +0200

    Delete 01. List-Of-Items directory

--- 01. List-Of-Items/app.js
+++
@@ -1,8 +0,0 @@
-function addItem() {
-   const input = document.getElementById('newItemText').value;
-   
-       const liElement = document.createElement('li');
-       liElement.textContent=input;
-       document.getElementById('items').appendChild(liElement);
-   
-}
